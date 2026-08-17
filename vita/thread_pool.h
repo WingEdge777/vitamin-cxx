82ee3ff5c80c443e06ef0b6b0d890f09d8220d8d
WingEdge777 <handkodu@gmail.com>
2026-08-17 14:11:28 +0800

    add thread pool

---
+++ vita/thread_pool.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <tuple>
+#include <vector>
+
+class ThreadPool {
+public:
+    explicit ThreadPool(int threads = 8) : stop_(false) {
+        for (int i = 0; i < threads; i++) {
+            workers_.emplace_back([this] {
+                for (;;) {
+                    std::function<void()> task;
+                    {
+                        std::unique_lock<std::mutex> lock(mtx_);
+                        cv_.wait(lock, [this] { return stop_ || !tasks_.empty(); });
+                        if (stop_ && tasks_.empty()) {
+                            return;
+                        }
+                        task = std::move(tasks_.front());
+                        tasks_.pop();
+                    }
+                    task();
+                }
+            });
+        }
+    }
+
+    template <class F, class... Args>
+    auto enqueue(F &&f, Args &&...args) -> std::future<std::invoke_result_t<F, Args...>> {
+        using ret_type = std::invoke_result_t<F, Args...>;
+        auto task = std::make_shared<std::packaged_task<ret_type()>>(
+            [func = std::forward<F>(f), tup = std::make_tuple(std::forward<Args>(args)...)]() mutable {
+                return std::apply(std::move(func), std::move(tup));
+            });
+        std::future<ret_type> res = task->get_future();
+        {
+            std::lock_guard<std::mutex> lock(mtx_);
+            if (stop_) {
+                std::cerr << "enqueue on stopped ThreadPool\n";
+            }
+            tasks_.emplace([task] { (*task)(); });
+        }
+        cv_.notify_one();
+        return res;
+    }
+
+    ~ThreadPool() {
+        {
+            std::lock_guard<std::mutex> lock(mtx_);
+            stop_ = true;
+        }
+        cv_.notify_all();
+        for (std::thread &worker : workers_) {
+            if (worker.joinable()) {
+                worker.join();
+            }
+        }
+    }
+
+    ThreadPool(const ThreadPool &) = delete;
+    ThreadPool &operator=(const ThreadPool &) = delete;
+
+private:
+    std::vector<std::thread> workers_;
+    std::queue<std::function<void()>> tasks_;
+
+    std::mutex mtx_;
+    std::condition_variable cv_;
+    bool stop_;
+};
